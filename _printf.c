b0ebaee614803c1d586b54920094903047f9a2e9
Crystal-holberton <9188@holbertonstudents.com>
2024-11-27 06:54:45 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -69,10 +69,8 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			i++;
-			if (!format[i])
+			if (!format[i + 1])
 			{
-				va_end(args);
 				return (-1);
 			}
 			count += handle_form(format[++i], args);
